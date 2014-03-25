9a5cc2ae9ddedbb80d357d33d5eb73c48dec7bce
Nils Martinussen <nilsma231@gmail.com>
2014-03-25 07:33:12 +0100

    added javascript and hide/show of details-text

---
+++ html/js/main.js
@@ -0,0 +1,55 @@
+/**
+ * a function that shows the paragraph span elements of class 'hidden'
+ * by changing the css display value to 'block', and also toggles the
+ * show/hide details link in the article accordingly
+ */
+function showDetails() {
+    var elements = getElements();
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].style.display='block';
+    }
+    toggleLink();
+}
+
+/**
+ * a function that hides the paragraph span elements of class 'hidden'
+ * by changing the css display value to 'none', and also toggles the
+ * show/hide details link in the article accordingly
+ */
+function hideDetails() {
+    var elements = getElements();
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].style.display='none';
+    }
+    toggleLink();
+}
+
+/**
+ * a function that toggles the display of the show/hide details link
+ * in the article according to the present state of that link:
+ * the link that is currently displayed will be hidden, and vice versa
+ */
+function toggleLink() {
+    var hideLink = document.getElementById('hideDetails');
+    var showLink = document.getElementById('showDetails');
+    var computedStyle = getComputedStyle(hideLink, null);
+    if(computedStyle.display == 'block') {
+        hideLink.style.display='none';
+        showLink.style.display='block';
+    } else {
+        hideLink.style.display='block';
+        showLink.style.display='none';
+    }
+}
+
+/**
+ * a function to get the paragraph span elements of class 'hidden'
+ * @returns {NodeList} elements - an array of elements of class 'hidden'
+ */
+function getElements() {
+    return document.getElementsByClassName('details');
+}
+
+window.onload = function() {
+    hideDetails();
+}
